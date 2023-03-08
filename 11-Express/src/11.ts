04e55a654b0d83be5abc3971c080bf8bfb699558
dizars1776 <dizars1776@yahoo.gr>
2023-03-08 17:52:25 +0200

    add 11

---
+++ 11-Express/src/11.ts
@@ -0,0 +1,54 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import 'express-async-errors'
+import morgan from 'morgan'
+
+const app = express()
+const port = process.env.PORT
+
+app.use(morgan('dev'))
+app.use(express.json())
+
+type Planet = {
+  id: number
+  name: string
+}
+
+type Planets = Planet[]
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: 'Earth',
+  },
+  {
+    id: 2,
+    name: 'Mars',
+  },
+]
+
+app.get('/', (req, res) => res.status(200).send({ message: 'Hello World!' }))
+
+app.get('/api/planets', (req, res) => res.status(200).json(planets))
+
+app.get('/api/planets/:id', (req, res) => {
+  const { id } = req.params
+  const planet = planets.find((p) => p.id === Number(id))
+
+  res.status(200).json(planet)
+})
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+)
+
+app.post('/api/planets', (req, res) => {
+  const { id, name } = req.body
+  const newPlanet = { id, name }
+  planets = [...planets, newPlanet]
+
+  console.log(req.body)
+
+  res.status(201).json({ message: 'The planet was created!' })
+})
